ca4816fa3b3c4b5fe82996bda8fdb37534d81b03
jawad <jawad.a@rocketbots.io>
2024-12-05 22:08:24 +0800

    Added sample contact form

---
+++ app/components/ContactForm.jsx
@@ -0,0 +1,19 @@
+import Form from './Form';
+
+export default function ContactForm() {
+  return (
+    <>
+      <div className="bg-[url('/mainimage.png')] bg-contain bg-no-repeat w-screen h-screen">
+        <div className="place-content-center grid grid-flow-row">
+          <div className="flex flex-col items-center pt-12 text-7xl text-white">
+            <h1 className="bg-clip-text bg-gradient-to-r from-[#FCD678] via-[#FCD678] to-[#BC8C2F] text-transparent">
+              Lorem ipsum dolor
+            </h1>
+            <h1>sit amet tosik</h1>
+            <Form />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image';
-import Header from './components/header';
+import Header from './components/Header';
+import ContactForm from './components/ContactForm';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <ContactForm />
     </>
   );
 }
